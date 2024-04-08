bf54eed50fac45fdc6b06dd07951d232fe117160
Andy Chu <andy.chu@jkos.com>
2024-04-08 09:23:00 +0800

    add logback, add example on jpe444

--- src/main/java/im/andychu/learn/jdk21lts/JEP444.java
+++ src/main/java/im/andychu/learn/jdk21lts/JEP444.java
@@ -1,13 +1,33 @@
 package im.andychu.learn.jdk21lts;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
 /**
  * <a href="https://openjdk.org/jeps/444">
  *  JEP 444: Virtual Threads
  * </a>
  * @author andy.chu 2024/4/6
  */
+@Slf4j
 public class JEP444 {
-    public static void main(String[] args) {
-        throw new UnsupportedOperationException("TODO");
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        try(var executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            var future1 = executor.submit(() -> "Hello");
+            var future2 = executor.submit(() -> "World");
+            log.info("{} {}", future1.get(), future2.get());
+        }
+
+        Thread.Builder ofVirtual = Thread.ofVirtual().name("duke-", 0);
+        ofVirtual.start(() -> log.info("apple"));
+
+        Thread.startVirtualThread(() -> log.info("banana"));
+
+        ThreadFactory factory = Thread.ofVirtual().name("work-", 0).factory();
+        factory.newThread(() -> log.info("candy")).start();
+        factory.newThread(() -> log.info("donut")).start();
     }
 }
